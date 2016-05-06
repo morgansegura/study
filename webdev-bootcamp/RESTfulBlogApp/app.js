47650418093d2512afbaf3d7e5cf5025e950abad
Morgan Segura <morgansegura@gmail.com>
2016-05-06 11:21:56 -0700

    complete RESTful Blog App

--- webdev-bootcamp/RESTfulBlogApp/app.js
+++ webdev-bootcamp/RESTfulBlogApp/app.js
@@ -1,7 +1,9 @@
-var bodyParser  = require("body-parser")
-mongoose        = require("mongoose"),
-express         = require("express"),
-app             = express();
+var bodyParser    = require("body-parser")
+mongoose          = require("mongoose"),
+express           = require("express"),
+expressSanitizer  = require("express-sanitizer"),  
+methodOverride    = require("method-override"),    
+app               = express();
 
 // setup a DB
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -11,6 +13,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 // Extend body-parser for GET/Send requests
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 // Define a mongoose schema for the blog
 var blogSchema = new mongoose.Schema({
@@ -44,6 +48,7 @@ app.get("/blogs/new", function(req, res){
 // Create Route
 app.post("/blogs", function(req, res){
    // create blog 
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -52,6 +57,51 @@ app.post("/blogs", function(req, res){
         }
     });
 });
+// Show route
+app.get("/blogs/:id", function(req, res ){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// Edit
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+               res.render("edit", {blog: foundBlog}); 
+        }
+    });    
+
+});
+// Update route
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);            
+        }
+    });
+});
+// Destroy
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");            
+        }
+    });
+});
+
+
 // Open Server at port 8080
 // var port = process.env.PORT, process.env.IP; // production port
 var port = 8080; // develop port
