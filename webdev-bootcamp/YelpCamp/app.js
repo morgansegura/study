41ba44c21b82663703aad7c25f20951c542fba0a
Morgan Segura <morgansegura@gmail.com>
2016-05-11 16:33:18 -0700

    Updating master branch

--- webdev-bootcamp/YelpCamp/app.js
+++ webdev-bootcamp/YelpCamp/app.js
@@ -21,7 +21,10 @@ var indexRoutes = require("./routes/index"),
 /*====
  ==== SETP THE DB
  ====*/
-mongoose.connect("mongodb://localhost/yelp_camp_v4");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v4";
+mongoose.connect(url);
+// mongoose.connect("mongodb://localhost/yelp_camp_v4");
+// mongoose.connect("mongodb://morgansegura:SeGuRa-1980mongolabyelpcamp@ds021182.mlab.com:21182/yelpcamp");
 
 /*====
  ==== SETUP BODY PARSER
