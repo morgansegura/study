b84c32357cbe496fb6caa74f79aee25c2bf3eae9
Morgan Segura <morgansegura@gmail.com>
2016-11-28 15:52:07 -0800

    paddle with ball

--- games/ball-with-mouse-paddle/script.js
+++ games/ball-with-mouse-paddle/script.js
@@ -1,14 +1,82 @@
-var canvas, canvasContext();
+
+var ballX = 75;
+var ballY = 75;
+var ballSpeedX = 5;
+var ballSpeedY = 7;
+
+const PADDLE_WIDTH = 100;
+const PADDLE_THICKNESS = 10;
+const PADDLE_DIST_FROM_EDGE = 60;
+var paddleX = 400;
+
+
+var canvas, canvasContext;
+
+function updateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    // var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    paddleX = mouseX - PADDLE_WIDTH/2;
+}
 
 window.onload = function(){
-    canvas.getElementById('gameCanvas');
+    canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
 
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0,0, canvas.width, canvas.height);
-
-    canvasContext.fillStyle = 'white';
-    canvasContext.beginPath();
-    canvasContext.arc(100,100,10,0,Math.PI*2, true);
-    canvasContext.fill();
+    var framesPerSecond = 30;
+    setInterval(updateAll, 1000/framesPerSecond);
+    canvas.addEventListener('mousemove', updateMousePos)
 };
+
+function updateAll() {
+    moveAll();
+    drawAll();
+}
+
+function ballReset() {
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
+
+function moveAll() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if( ballX < 0 ) { // left
+        ballSpeedX *= -1;
+    }
+
+    if( ballX > canvas.width ) { //right
+        ballSpeedX *= -1;
+    }
+    if( ballY < 0 ) { // top
+        ballSpeedY *= -1;
+    }
+
+    if( ballY > canvas.height ) { // bottom
+        ballReset();
+        // ballSpeedY *= -1;
+    }
+}
+function drawAll() {
+
+    colorRect(0,0, canvas.width,canvas.height, 'black'); // clear screen
+    colorCircle(ballX,ballY, 10, 'white'); // draw ball
+
+    colorRect(paddleX, canvas.height-PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS)
+
+}
+
+function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor) {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
+}
+function colorCircle(centerX,centerY, radius, fillColor) {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX,centerY, radius,0,Math.PI*2, true);
+    canvasContext.fill();
+}
