31b2c9db24be2ed27872ea6f8d6ce4c3d318438a
Morgan Segura <morgansegura@gmail.com>
2016-12-08 17:06:00 -0800

    working

--- advancedjs/app/assets/js/scripts.js
+++ advancedjs/app/assets/js/scripts.js
@@ -11,7 +11,31 @@
   'use strict';
 
   $(function () {
-    // FastShell
+    // My quiz and notes
+	var notesData = [{
+		headline:"Headline 1",
+		data: {
+			240:"link",
+			360:"link"
+		}
+	},{
+		headline:"Headline 2",
+		data: {
+			240:"link",
+			360:"link"
+		}
+	}];
+
+	notesData.forEach(function(notes){
+		var headline = $('<h4 class="notes headline">' + notes.headline + '<h4>'),
+			data = $('<li>' + notes.data +'</li>'),
+			html = '';
+
+		$('.main').append(headline);
+		html += '<ul>'
+		$('.notes.headline').after(data)
+		html += '</ul>'
+	});
   });
 
 })(jQuery, window, document);
