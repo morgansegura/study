5ce55825141d0358d14101b524d6b26dd13b01f8
Morgan Segura <morgansegura@gmail.com>
2016-12-09 15:58:46 -0800

    update with content

--- advancedjs/app/assets/js/app.js
+++ advancedjs/app/assets/js/app.js
@@ -30,23 +30,63 @@ ajs.config(function($stateProvider){
 
 // Main controller
 ajs.controller('MainController', function($scope) {
-	$scope = {
-		default: 'Home'
-	}
+	$scope.headline = 'This is the Home page';
 });
 
 
 // Main controller
 ajs.controller('NotesController', function($scope) {
-	$scope = {
-		default: 'Notes'
-	}
+	// Main headline
+	$scope.headline = 'This is the Notes page';
+	// Notes body content
+	$scope.notes = [
+		{
+			topic: 'What is "use strict;" and what does it do?',
+			answer: '"use strict;" allows you to place a program or a function into a strict operating context.',
+			pros: {
+				1: 'It makes debugging easier',
+				2: 'Code errors that would have otherwise been ignored or failed silently, will now generate an error or throw an exception.',
+				3: 'He smiles',
+				4: 'She says I love you',
+			},
+			cons: {
+				1: 'He walks the dog',
+				2: 'She laughs',
+				3: 'He smiles',
+				4: 'She says I love you',
+			},
+			sideNote: {
+				1: 'Side Note 1',
+				2: 'Side Note 2'
+			},
+			quotes: {
+				1: 'Quote 1',
+				2: 'Quote 2',				
+			},
+		},
+		{
+			topic: 'Why is the sky blue?',
+			bullets: {
+				1: 'He walks the dog',
+				2: 'She laughs',
+				3: 'He smiles',
+				4: 'She says I love you',
+			},
+			sideNote: {
+				1: 'Side Note 1',
+				2: 'Side Note 2'
+			},
+			quotes: {
+				1: 'Quote 1',
+				2: 'Quote 2',				
+			} 
+		} 
+
+	];
 });
 
 
 // Quizzes controller
 ajs.controller('QuizzesController', function($scope) {
-	$scope = {
-		default: 'Quizzes'
-	}
+	$scope.headline = 'This is the Quizzes page';
 });
